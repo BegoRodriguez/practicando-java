5003eca046c59aea8ce795c3531534de3217c5f1
dani10garcia <dani10garcia1017@gmail.com>
2024-01-11 15:43:47 +0100

    prueba

--- 02 - TiposPrimitivosyVariables/TiposPrimitivos.java
+++ 02 - TiposPrimitivosyVariables/TiposPrimitivos.java
@@ -45,6 +45,12 @@ class TiposPrimitivos {
             int j = i + 1;  // Cuando un entero se desborda, vuelve al valor mínimo y comienza a contar desde allí.
             System.out.println("Valor maximo entero: " + i + " Operación desbordada " + j); // La variable 'j' volverá a -2147483648
             
+  /* Desbordamiento (underflow) - Ocurre cuando sobrepasamos el limite de nuestras primitivas */
+            int q = Integer.MIN_VALUE;
+            int p = q - 1;  // Cuando un entero se desborda, vuelve al valor mínimo y comienza a contar desde allí.
+            System.out.println("Valor minimo entero: " + q + " Operación desbordada " + p); // La variable 'j' volverá a -2147483648
+            
+
             double d = Double.MAX_VALUE;
             double o = d + 1; // Los números en coma flotante se desbordan devolviendo el valor máximo
             System.out.println("Valor maximo double: " + d + " Operación desbordada " + o);  // La variable 'o' será el valor máximo
